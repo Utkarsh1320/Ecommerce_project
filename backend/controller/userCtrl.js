a6691bd8adc04ac4d28aaa08dd5dc6cdce31ac49
Utkarsh More <utkarsh.more.2000@gmail.com>
2024-03-06 09:45:34 -0500

    backend till sprint 2

--- backend/controller/userCtrl.js
+++ backend/controller/userCtrl.js
@@ -9,18 +9,15 @@ const createUser = asyncHandler(async (req, res) => {
         //create a new user
         const newUser = await User.create(req.body);
         res.json(newUser);
-
     } else {
-        //User alredy exisit
-        throw new Error('User Already Exists');
-        
+        //User already exists
+        throw new Error('User Already Exists');   
     }
 });
 
-
 const loginUserCtrl = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-    //check if user exisit 
+    //check if user exists
     const findUser = await User.findOne({ email });
     if (findUser && (await findUser.isPasswordMatched(password))) {
         res.json({
@@ -36,4 +33,72 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = {createUser , loginUserCtrl};
+const resetPassword = asyncHandler(async (req, res) => {
+    const { email, newPassword } = req.body;
+    const findUser = await User.findOne({ email });
+    if (!findUser) {
+        throw new Error('User Not Found');
+    } else {
+        // Check if newPassword is provided
+        if (!newPassword) {
+            throw new Error('New password is required');
+        }
+        findUser.password = newPassword;
+        await findUser.save();
+        res.json({ message: "Password updated successfully" });
+    }
+});
+
+const getallUser = asyncHandler(async (req, res) => {
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const getaUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const getaUser = await User.findById(id);
+        res.json({
+            getaUser,
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const updatedUser = asyncHandler(async (req, res) => {
+    
+    const { id } = req.params;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, {
+            firstName: req?.body?.firstName,
+            lastName: req?.body?.lastName,
+            email: req?.body?.email,
+            mobile: req?.body?.mobile,
+        }, {
+            new:true,
+        }
+        );
+        res.json(updatedUser);
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+
+const deleteaUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleteaUser = await User.findByIdAndDelete(id);
+        res.json({
+            deleteaUser,
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+module.exports = { createUser, loginUserCtrl, getallUser , getaUser, deleteaUser, updatedUser};
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require('mongoose'); 
 const bcrypt = require('bcrypt');
-// Declare the Schema of the Mongo model
+
 var userSchema = new mongoose.Schema({
     firstName:{
         type:String,
